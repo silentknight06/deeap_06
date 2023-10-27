f7a1e74f070622da9f083299e1693ff4a08cfc0a
silentnight06 <deepak7393@gmail.com>
2023-10-27 20:15:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navigation from './component/Navigation'
+import Hero from './component/Hero'
+import './style.css'
+const App = () => {
+  return (
+   <>
+   <Navigation/>
+   <Hero/>
+
+   </>
+  )
+}
+
+export default App
+
+
